1f5d76971110cdd8c0f6befab19dbcf38c3f94f7
Orlang00r <Orlangur00r@yandex.ru>
2022-04-14 21:06:45 +0400

    Proverka na kvadrat

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Input new integer number");
+﻿
+/* Задача 1. 
+Console.WriteLine("Input new integer number");
 int num = Convert.ToInt32(Console.ReadLine());
 int Quad = num*num;
 Console.WriteLine("result is" + Quad);
+*/
+
+/* Задача 2.
+Console.WriteLine("Input first number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 * num1 == num2)
+    Console.WriteLine("Второе число является квадратом первого");
+else
+    Console.WriteLine("Второе не число является квадратом первого");
+    */
