e0bc51f387a49ca01ee95e657eb2e2aaf8a83104
Orlang00r <Orlangur00r@yandex.ru>
2022-04-25 11:51:58 +0400

    Sem2

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int Shownumber(int n)
+{
+    
+    int firstnum = n / 10;
+    int secondnum = n % 10;
+
+
+    Console.Write(n + "->");
+
+    if (firstnum > secondnum)
+    {
+        return firstnum;
+    }
+    else
+    {
+        return secondnum;
+    }
+}
+int num = new Random().Next(10, 99);
+int a = Shownumber(num);
+Console.WriteLine(a);
