ee10ece0b2076bd8474fafb6993b8669a9979399
Orlang00r <Orlangur00r@yandex.ru>
2022-05-01 18:27:09 +0400

    sum of number

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int NaturalSum(int a)
+{
+    int sum = 0;
+
+    if (a < 1) return 0;
+    else
+    {
+        for (int i = 1; i <= a; i++) sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NaturalSum(num));
