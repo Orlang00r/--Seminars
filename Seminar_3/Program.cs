eed9b61dd9bc88151edbc1b876018dba0b2c97db
Orlang00r <Orlangur00r@yandex.ru>
2022-05-01 19:26:37 +0400

    Methods

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 int NaturalSum(int a)
 {
     int sum = 0;
@@ -14,3 +14,51 @@ int NaturalSum(int a)
 Console.WriteLine("Input number ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(NaturalSum(num));
+*/
+
+
+//Создание случайного массива.
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+// Возврат значений массива
+
+void ShowArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+void FindSumOfElements(int[] array)
+{
+    int plussum = 0;
+    int minussum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            plussum += array[i];
+        else 
+            minussum += array[i];
+    }
+    Console.WriteLine ("Sum of positive elements " + plussum);
+    Console.WriteLine ("Sum of negative elements " + minussum);
+}
+
+
+int[] myArray = CreateRandomArray(6, -5, 5);
+ShowArray(myArray);
+FindSumOfElements(myArray);
